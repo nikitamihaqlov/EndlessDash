4074be8d8119516208a9313111afa6e491482e22
Nikita Mikhailov <nikitamihaqlov@gmail.com>
2025-02-14 23:47:28 +0300

    Add road repeat

--- Assets/Scripts/Environment.cs
+++ Assets/Scripts/Environment.cs
@@ -4,8 +4,17 @@ public class Environment : MonoBehaviour
 {
     private float _speed = 10;
 
+    [SerializeField] private Transform _returnTransform;
+
+    private const float _zBound = -10;
+
     void Update()
     {
         transform.Translate(Vector3.back * Time.deltaTime * _speed);
+
+        if (transform.position.z < _zBound)
+        {
+            transform.position = _returnTransform.position;
+        }
     }
 }
