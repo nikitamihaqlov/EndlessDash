12cc44d61aebf60a379bb787abdff5e296474f3c
Nikita Mikhailov <nikitamihaqlov@gmail.com>
2025-02-19 21:36:05 +0300

    Make game manager scriptable object

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,22 +1,9 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+[CreateAssetMenu]
+public class GameManager : ScriptableObject
 {
-    public static GameManager Instance;
-
     private float m_Speed = 3f;
 
     public float Speed => m_Speed;
-
-    private void Awake()
-    {
-        if (Instance == null)
-        {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
 }
