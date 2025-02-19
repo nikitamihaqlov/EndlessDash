7476267105ffaaa8cea2ef5fdf860eae1e9acb59
Nikita Mikhailov <nikitamihaqlov@gmail.com>
2025-02-19 23:13:04 +0300

    [Refactoring] Make movement MonoBehaviour

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 
-public class Movement
+public class Movement : MonoBehaviour
 {
+    [SerializeField] private GameManager m_GameManager;
+
+    private void Update()
+    {
+        transform.position = UpdatePosition(transform.position, m_GameManager.Speed);
+    }
+
     public Vector3 UpdatePosition(Vector3 position, float speed)
     {
         var target = position + Vector3.back;
--- Assets/Scripts/RoadSection.cs
+++ Assets/Scripts/RoadSection.cs
@@ -2,24 +2,12 @@ using UnityEngine;
 
 public class RoadSection : MonoBehaviour
 {
-    private Movement m_Movement;
-    [SerializeField] private GameManager m_GameManager;
     private Road m_Road;
 
     public Road Road
     {
         get => m_Road;
         set => m_Road = value;
-    }
-
-    private void Start()
-    {
-        m_Movement = new Movement();
-    }
-
-    private void Update()
-    {
-        transform.position = m_Movement.UpdatePosition(transform.position, m_GameManager.Speed);
     }
 
     private void OnTriggerEnter(Collider other)
